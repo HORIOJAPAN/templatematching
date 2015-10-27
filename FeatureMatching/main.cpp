4c166935f9cf6a4c217e94739f7a68111b5a53dc
zakiyama124 <s14mh124@mail.saitama-u.ac.jp>
2015-10-27 21:09:11 +0900

    がぞううｐ

--- FeatureMatching/main.cpp
+++ FeatureMatching/main.cpp
@@ -7,13 +7,13 @@
 
 using namespace cv;
 
-int		ideal_x = 635;
-int		ideal_y = 464;
-float	Angle = 10.0;
-float	D = 0.0;
+int		ideal_x = 0.0;
+int		ideal_y = 0.0;
 double	maxValue = 0;
+float	Angle = 0.0;
+float	D = 0.0;
 
-# define _Evaluate 0 
+# define _Evaluate 1
 // 0:テンプレートマッチングのみ  1:評価値計算
 
 // # define _ImageDirectory "/img
@@ -21,6 +21,49 @@ double	maxValue = 0;
 // # define _ImageMatch "/img/a001.jpg"
 
 
+
+void Localization(		const cv::Mat img1,			// 画像１のファイル名
+						const cv::Mat img2,			// 画像２のファイル名
+						float angle_center,			// 幅の中心
+						float angle_width,			// 角度幅
+						float angle_shredded		// 刻み角度				
+						)
+{
+	// テンプレートマッチングと評価値の算出・最適値の出力
+
+	Mat match;
+	Mat kaitenImg;
+	Point Pt;
+	double maxVal;		// マッチング率の最大値
+
+	for (float i = (angle_center)-(angle_width); i < ((angle_center) + (angle_width) + 1) ; i += angle_shredded){
+		// 回転：  [deg]
+		float angle = i;
+		// 大きさ：  [倍]
+		float scale = 1.0;
+		// 画像の中心を求める
+		Point2f center(img2.cols / 2.0, img2.rows / 2.0);
+		// 回転行列
+		Mat matrix = cv::getRotationMatrix2D(center, angle, scale);
+		// 画像を回転
+		warpAffine(img2, kaitenImg, matrix, img2.size());
+		// ウィンドウ表示
+		imshow("kaitenImg", kaitenImg);
+		// マッチング
+		matchTemplate(img1, kaitenImg, match, CV_TM_CCOEFF_NORMED);
+		// 相関値を求める
+		minMaxLoc(match, NULL, &maxVal, NULL, &Pt);
+
+		printf("%d\t%lf\t%d,%d\n", (int)i, maxVal, Pt.x, Pt.y);
+
+		if (maxVal > maxValue){
+			ideal_x = Pt.x;
+			ideal_y = Pt.y;
+			maxValue = maxVal;
+			Angle = angle;
+		}
+	}
+}
 
 void MatchingEvaluation(	const cv::Mat img1,			// 画像１のファイル名
 							const cv::Mat img2,			// 画像２のファイル名
@@ -37,9 +80,8 @@ void MatchingEvaluation(	const cv::Mat img1,			// 画像１のファイル名
 	float distance;		// 理想座標とマッチング座標の相対距離
 	float Evaluation;	// 評価値
 	double maxVal;		// マッチング率の最大値
-	double maxVal_1 = 0, maxVal_2 = 0;
 
-	for (float i = (angle_center)-(angle_width); i < ((angle_center) + (angle_width) + 1) ; i += angle_shredded){
+	for (float i = (angle_center)-(angle_width); i < ((angle_center)+(angle_width)+1); i += angle_shredded){
 		// 回転：  [deg]
 		float angle = i;
 		// 大きさ：  [倍]
@@ -71,20 +113,13 @@ void MatchingEvaluation(	const cv::Mat img1,			// 画像１のファイル名
 
 		printf("%d\t%lf\t%d,%d \t%d\t%lf\n", (int)i, maxVal, Pt.x, Pt.y, (long)distance, Evaluation);
 
-# if _Evaluate == 0
-		if (maxVal > maxValue){
-			maxValue = maxVal;
-			Angle = angle;
-		}
-# else
 		if (Evaluation > 0){
-			maxValue = maxVal;
 			if (D < Evaluation){
-				D = Evaluation;
+				maxValue = maxVal;
 				Angle = angle;
+				D = Evaluation;
 			}
 		}
-# endif
 	}
 }
 
@@ -98,27 +133,27 @@ int main()
 	Mat match;	
 	Mat kaitenImg;
 	Point Pt;
-	Point Pt2;
 	Point maxPt;
 	float ex_angle = 0.0;
 	float distance;
-	float distance_point;
-	double minVal , maxVal;
-	double minValue = 0 , maxValue = 0;
-	double maxVal_1 = 0 , maxVal_2 = 0;
-	int count = 0;
-
-	// img1の画像の領域を指定
-	Mat sub = img1(Rect(0, 0, 1000,1000));
-	imshow("Img", img2);
-
-	int n = 5 ;
-	printf("%d度ずつ刻んでマッチ開始\n" , n);
-	printf("度\t相関値\t\tx,y\t距離\t評価値\n");
+	double maxVal;
+	double maxValue = 0;
 
 	clock_t start = clock();
 
-	//MatchingEvaluation(sub,img2,0,30,n);
+	// img1の画像の領域を指定
+	Mat sub = img1(Rect(0, 0, 1000,1000));
+	//imshow("Img", img2);
+
+	int m = 3;
+	printf("自己位置を見つけています...\n", m);
+	Localization(sub, img2, 0, 30, m);
+	printf("%f\t%lf\t%d,%d\n", Angle, maxValue, ideal_x, ideal_y);
+
+	int n = 5;
+	printf("%d度ずつ刻んでマッチ開始\n" , n);
+	printf("度\t相関値\t\tx,y\t距離\t評価値\n");
+	MatchingEvaluation(sub, img2, 0, 30, n);
 
 # if _Evaluate
 	if (D == 0.0){
@@ -143,8 +178,7 @@ int main()
 	warpAffine(img2, kaitenImg, matrix, img2.size());
 	imshow("kaitenImg", kaitenImg);
 	matchTemplate(sub, kaitenImg, match, CV_TM_CCOEFF_NORMED);
-	minMaxLoc(match, &minValue, &maxValue, NULL, &maxPt);
-	
+	minMaxLoc(match, NULL, &maxValue, NULL, &maxPt);
 	rectangle(sub, maxPt, Point(maxPt.x + kaitenImg.cols, maxPt.y + kaitenImg.rows), Scalar(0, 0, 255), 2, 8, 0);
 	printf("Angle = %f position.x = %d position.y = %d \n", angle, maxPt.x , maxPt.y);
 	printf("%ld\n" , clock()-start);
