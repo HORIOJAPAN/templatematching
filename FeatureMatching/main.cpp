0d83a8cd56160bd3f36db3b514fc234c7b71f0e4
zakiyama124 <s14mh124@mail.saitama-u.ac.jp>
2015-10-27 19:29:12 +0900

    updata

--- FeatureMatching/main.cpp
+++ FeatureMatching/main.cpp
@@ -1,130 +1,39 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/opencv_lib.hpp>
 #include <opencv2/nonfree/nonfree.hpp> // SIFT・SURFモジュール用
+#include <time.h>
 
 using namespace cv;
 
-void FeatureMatching(
-	const std::string& filename1,			// 画像１のファイル名
-	const std::string& filename2,			// 画像２のファイル名
-	const std::string& featureDetectorName,		// detectorType
-	const std::string& descriptorExtractorName,  // descriptorExtractorType
-	const std::string& descriptorMatcherName,	// descriptorMatcherType
-	bool crossCheck = false
-	)				// マッチング結果をクロスチェックするかどうか
-{
-	// 画像の読み込み
-	cv::Mat img1 = cv::imread(filename1);
-	cv::Mat img2 = cv::imread(filename2);
-	Mat src1, src2, dst1, dst2, color_dst1, color_dst2;
+int		ideal_x = 635;
+int		ideal_y = 464;
+float	Angle = 10.0;
+float	D = 0.0;
+double	maxValue = 0;
 
-	GaussianBlur(img1, img1, Size(5, 5), 0, 0);
-	GaussianBlur(img2, img2, Size(5, 5), 0, 0);
-	src1 = img1.clone();
-	src2 = img2.clone();
-	Canny(src1, dst1, 50, 200, 3);
-	cvtColor(dst1, color_dst1, CV_GRAY2BGR);
-	Canny(src2, dst2, 50, 200, 3);
-	cvtColor(dst2, color_dst2, CV_GRAY2BGR);
-	color_dst1 = Mat(Size(img1.cols, img1.rows), CV_8U, Scalar::all(0));
-	color_dst2 = Mat(Size(img2.cols, img1.rows), CV_8U, Scalar::all(0));
-	
-	vector<Vec4i> lines1;
-	HoughLinesP(img1, lines1, 1, CV_PI / 180, 8, 5, 0);
-	for (size_t i = 0; i < lines1.size(); i++)
-	{
-		line(color_dst1, Point(lines1[i][0], lines1[i][1]),
-			Point(lines1[i][2], lines1[i][3]), Scalar(0, 0, 255));
-	}
-	/*
-	vector<Vec4i> lines2;
-	HoughLinesP(img2, lines2, 10, CV_PI / 180, 8, 5, 0);
-	for (size_t i = 0; i < lines2.size(); i++)
-	{
-		line(color_dst2, Point(lines2[i][0], lines2[i][1]),
-			Point(lines2[i][2], lines2[i][3]), Scalar(0, 0, 255));
-	}*/
-	
-	// SIFT・SURFモジュールの初期化
-	cv::initModule_nonfree();
+# define ai
 
-	printf("A");
-
-	// 特徴点抽出
-	cv::Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create(featureDetectorName);
-	std::vector<cv::KeyPoint> keypoint1, keypoint2;
-	detector->detect(img1, keypoint1);
-	detector->detect(img2, keypoint2);
-	//detector->detect(color_dst1, keypoint1);
-	//detector->detect(color_dst2, keypoint2);
-
-	printf("B");
-
-	// 特徴記述
-	cv::Ptr<cv::DescriptorExtractor> extractor = cv::DescriptorExtractor::create(descriptorExtractorName);
-	cv::Mat descriptor1, descriptor2;
-	extractor->compute(img1, keypoint1, descriptor1);
-	extractor->compute(img2, keypoint2, descriptor2);
-	//extractor->compute(color_dst1, keypoint1, descriptor1);
-	//extractor->compute(color_dst2, keypoint2, descriptor2);
-
-	printf("C");
-
-	// マッチング
-	cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(descriptorMatcherName);
-	std::vector<cv::DMatch> dmatch;
-	/*if (crossCheck)
-	{
-	// クロスチェックする場合
-	std::vector<cv::DMatch> match12, match21;
-	matcher->match(descriptor1, descriptor2, match12);
-	matcher->match(descriptor2, descriptor1, match21);
-	for (size_t i = 0; i < match12.size(); i++)
-	{
-	cv::DMatch forward = match12[i];
-	cv::DMatch backward = match21[forward.trainIdx];
-	if (backward.trainIdx == forward.queryIdx)
-	dmatch.push_back(forward);
-	}
-	}
-	else
-	{
-	// クロスチェックしない場合
-	matcher->match(descriptor1, descriptor2, dmatch);
-	}*/
-
-	// マッチング結果の表示
-	cv::Mat out;
-	cv::drawMatches(img1, keypoint1, img2, keypoint2, dmatch, out);
-	//cv::drawMatches(color_dst1, keypoint1, color_dst2, keypoint2, dmatch, out);
-	cv::imshow("matching", out);
-	while (cv::waitKey(1) == -1);
-
-	printf("\n終了");
-
-}
-
-int main()
+void MatchingEvaluation(
+	const cv::Mat img1,			// 画像１のファイル名
+	const cv::Mat img2,			// 画像２のファイル名
+	float angle_center,			// 幅の中心
+	float angle_width,			// 角度幅
+	float angle_shredded		// 刻み角度
+	)
 {
 
-	Mat img1 = imread("rouka02.jpg");
-	Mat img2 = imread("rouka002.jpg");
-	Mat match;	
+	Mat match;
 	Mat kaitenImg;
 	Point Pt;
-	Point maxPt;
-	float Angle;
-	double minVal , maxVal;
-	double minValue = 0 , maxValue = 0;
+	float distance;
+	float Evaluation;
+	double maxVal;
+	double maxVal_1 = 0, maxVal_2 = 0;
 
-	imshow("Img", img2);
-
-	printf("開始\n");
-
-	for (int i = -5; i < 5; i += 1){
+	for (float i = (angle_center)-(angle_width); i < (angle_center) + (angle_width) + 1 ; i += angle_shredded){
 
 		// 回転：  [deg]
-		float angle = i ;
+		float angle = i;
 		// 大きさ：  [倍]
 		float scale = 1.0;
 		// 画像の中心を求める
@@ -137,28 +46,97 @@ int main()
 		// マッチング
 		matchTemplate(img1, kaitenImg, match, CV_TM_CCOEFF_NORMED);
 		// 相関値を求める
-		minMaxLoc(match, &minVal, &maxVal, NULL, &Pt);
-		//printf("%lf %lf\n", minVal, maxVal);
+		minMaxLoc(match, NULL, &maxVal, NULL, &Pt);
+		// エンコーダによる自己位置とマッチング後の自己位置の相対距離を求める
+		distance = sqrt(powf((Pt.x - ideal_x), 2) + powf((Pt.y - ideal_y), 2));
+
+		if (distance > 6){
+			Evaluation = maxVal - log10(distance) / 5;
+		}
+		else{
+			Evaluation = maxVal;
+		}
+
+		printf("%d\t%lf\t%d,%d \t%d\t%lf\n", (int)i, maxVal, Pt.x, Pt.y, (long)distance, Evaluation);
+
+# ifdef ai
 		if (maxVal > maxValue){
 			maxValue = maxVal;
-			minValue = minVal;
 			Angle = angle;
 		}
-		//printf( "%lf %lf\n", minValue, maxValue );
+# else
+		if (Evaluation > 0){
+			maxValue = maxVal;
+			if (D < Evaluation){
+				D = Evaluation;
+				Angle = angle;
+			}
+		}
+# endif
 	}
 
+}
+
+
+int main()
+{
+
+	Mat img1 = imread("C:/Users/user/Desktop/FeatureMatching/FeatureMatching/img/fieldMap2.jpg");
+	Mat img2 = imread("C:/Users/user/Desktop/FeatureMatching/FeatureMatching/img/c109.jpg");
+	Mat match;	
+	Mat kaitenImg;
+	Point Pt;
+	Point Pt2;
+	Point maxPt;
+	float ex_angle = 0.0;
+	float distance;
+	float distance_point;
+	double minVal , maxVal;
+	double minValue = 0 , maxValue = 0;
+	double maxVal_1 = 0 , maxVal_2 = 0;
+	int count = 0;
+
+	// img1の画像の領域を指定
+	Mat sub = img1(Rect(0, 0, 1000,1000));
+	imshow("Img", img2);
+
+	int n = 5 ;
+	printf("%d度ずつ刻んでマッチ開始\n" , n);
+	printf("度\t相関値\t\tx,y\t距離\t評価値\n");
+
+	clock_t start = clock();
+
+	//MatchingEvaluation(sub,img2,0,30,n);
+
+# ifndef ai
+	if (D == 0.0){
+		printf("%f\n", D);
+		printf("No matching\n");
+		return(0);
+	}
+# endif
+	
+	printf("1度ずつ刻んでマッチ開始\n");
+	printf("度\t相関値\t\tx,y\t\t距離\t評価値\n");
+
+	D = 0.0;
+	MatchingEvaluation(sub, img2, Angle, 5, 1);
+
+	//MatchingEvaluation(sub, img2, Angle, 0, 1);
+	
 	float angle = Angle;
 	float scale = 1.0;
 	Point2f center(img2.cols / 2.0, img2.rows / 2.0);
 	Mat matrix = cv::getRotationMatrix2D(center, angle, scale);
 	warpAffine(img2, kaitenImg, matrix, img2.size());
 	imshow("kaitenImg", kaitenImg);
-	matchTemplate(img1, kaitenImg, match, CV_TM_CCOEFF_NORMED);
+	matchTemplate(sub, kaitenImg, match, CV_TM_CCOEFF_NORMED);
 	minMaxLoc(match, &minValue, &maxValue, NULL, &maxPt);
-	rectangle(img1, maxPt, Point(maxPt.x + kaitenImg.cols, maxPt.y + kaitenImg.rows), Scalar(0, 0, 255), 2, 8, 0);
-	//rectangle(img1, maxPt, Point( maxPt.x , maxPt.y ), Scalar(0, 0, 255), 2, 8, 0);
-	printf("Angle = %f position.x = %d position.y = %d \n", Angle, maxPt.x , maxPt.y);
-	imshow("Image", img1);
+	
+	rectangle(sub, maxPt, Point(maxPt.x + kaitenImg.cols, maxPt.y + kaitenImg.rows), Scalar(0, 0, 255), 2, 8, 0);
+	printf("Angle = %f position.x = %d position.y = %d \n", angle, maxPt.x , maxPt.y);
+	printf("%ld\n" , clock()-start);
+	imshow("Image", sub);
 	waitKey(0);
 
 }
