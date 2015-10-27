93b7288af20ba7bc724d28144843a51af7ec5c53
zakiyama124 <s14mh124@mail.saitama-u.ac.jp>
2015-10-27 22:06:23 +0900

    相関値から自己位置推定

--- FeatureMatching/main.cpp
+++ FeatureMatching/main.cpp
@@ -7,13 +7,14 @@
 
 using namespace cv;
 
-int		ideal_x = 0.0;
-int		ideal_y = 0.0;
+int		ideal_x = 610;
+int		ideal_y = 460;
 double	maxValue = 0;
 float	Angle = 0.0;
 float	D = 0.0;
 
 # define _Evaluate 1
+// 0:自己位置を直接入力  1:相関値のみで自己位置を推定
 // 0:テンプレートマッチングのみ  1:評価値計算
 
 // # define _ImageDirectory "/img
@@ -54,7 +55,7 @@ void Localization(		const cv::Mat img1,			// 画像１のファイル名
 		// 相関値を求める
 		minMaxLoc(match, NULL, &maxVal, NULL, &Pt);
 
-		printf("%d\t%lf\t%d,%d\n", (int)i, maxVal, Pt.x, Pt.y);
+		//printf("%d\t%lf\t%d,%d\n", (int)i, maxVal, Pt.x, Pt.y);
 
 		if (maxVal > maxValue){
 			ideal_x = Pt.x;
@@ -63,6 +64,7 @@ void Localization(		const cv::Mat img1,			// 画像１のファイル名
 			Angle = angle;
 		}
 	}
+	printf("%f\t%lf\t%d,%d\n", Angle, maxValue, ideal_x, ideal_y);
 }
 
 void MatchingEvaluation(	const cv::Mat img1,			// 画像１のファイル名
@@ -129,14 +131,12 @@ int main()
 	//Mat img1 = imread(_ImageField);
 	//Mat img2 = imread(_ImageMatch);
 	Mat img1 = imread("C:/Users/user/Desktop/FeatureMatching/FeatureMatching/img/fieldMap2.jpg");
-	Mat img2 = imread("C:/Users/user/Desktop/FeatureMatching/FeatureMatching/img/c109.jpg");
+	Mat img2 = imread("C:/Users/user/Desktop/FeatureMatching/FeatureMatching/img/c106.jpg");
 	Mat match;	
 	Mat kaitenImg;
 	Point Pt;
 	Point maxPt;
 	float ex_angle = 0.0;
-	float distance;
-	double maxVal;
 	double maxValue = 0;
 
 	clock_t start = clock();
@@ -145,23 +145,24 @@ int main()
 	Mat sub = img1(Rect(0, 0, 1000,1000));
 	//imshow("Img", img2);
 
-	int m = 3;
+# if _Evaluate
+
+	int m = 5;
 	printf("自己位置を見つけています...\n", m);
-	Localization(sub, img2, 0, 30, m);
-	printf("%f\t%lf\t%d,%d\n", Angle, maxValue, ideal_x, ideal_y);
+	Localization(sub, img2, 0, 45, m);
+
+# endif
 
 	int n = 5;
 	printf("%d度ずつ刻んでマッチ開始\n" , n);
 	printf("度\t相関値\t\tx,y\t距離\t評価値\n");
-	MatchingEvaluation(sub, img2, 0, 30, n);
+	MatchingEvaluation(sub, img2, 0, 45, n);
 
-# if _Evaluate
 	if (D == 0.0){
 		printf("%f\n", D);
 		printf("No matching\n");
 		return(0);
 	}
-# endif
 	
 	printf("1度ずつ刻んでマッチ開始\n");
 	printf("度\t相関値\t\tx,y\t\t距離\t評価値\n");
@@ -169,17 +170,15 @@ int main()
 	D = 0.0;
 	MatchingEvaluation(sub, img2, Angle, 5, 1);
 
-	//MatchingEvaluation(sub, img2, Angle, 0, 1);
-
 	float angle = Angle;
 	float scale = 1.0;
 	Point2f center(img2.cols / 2.0, img2.rows / 2.0);
 	Mat matrix = cv::getRotationMatrix2D(center, angle, scale);
 	warpAffine(img2, kaitenImg, matrix, img2.size());
-	imshow("kaitenImg", kaitenImg);
 	matchTemplate(sub, kaitenImg, match, CV_TM_CCOEFF_NORMED);
 	minMaxLoc(match, NULL, &maxValue, NULL, &maxPt);
-	rectangle(sub, maxPt, Point(maxPt.x + kaitenImg.cols, maxPt.y + kaitenImg.rows), Scalar(0, 0, 255), 2, 8, 0);
+	rectangle(sub , maxPt, Point(maxPt.x + kaitenImg.cols, maxPt.y + kaitenImg.rows), Scalar(0, 0, 255), 2, 8, 0);
+	imshow("kaitenImg", kaitenImg);
 	printf("Angle = %f position.x = %d position.y = %d \n", angle, maxPt.x , maxPt.y);
 	printf("%ld\n" , clock()-start);
 	imshow("Image", sub);
